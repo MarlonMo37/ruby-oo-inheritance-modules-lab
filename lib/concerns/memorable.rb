f078dc89adb66bbc8eb1d717f3b126541861bced
MarlonMo37 <marlonmoreno894@gmail.com>
2020-11-17 17:01:46 -0600

    commit tree b7921a421df07c42bf58772f1211ea9f82c3ce0c

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+    # def self.reset_all
+    #     self.all.clear
+    # end
+
+    # def self.count
+    #     self.all.count
+    # end
+
+    def reset_all
+    all.clear
+  end
+
+  def count
+    all.count
+  end
+end
